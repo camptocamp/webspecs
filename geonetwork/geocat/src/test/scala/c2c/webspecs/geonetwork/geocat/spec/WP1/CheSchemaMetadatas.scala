c9d9efff97c842c512af46d1e23420c919280bd0
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-07-04 16:23:29 +0200

    test WP1
    ISO schema testing  using a new MD from geocat, and hardenning the existing test

--- geonetwork/geocat/src/test/scala/c2c/webspecs/geonetwork/geocat/spec/WP1/CheSchemaMetadatas.scala
+++ geonetwork/geocat/src/test/scala/c2c/webspecs/geonetwork/geocat/spec/WP1/CheSchemaMetadatas.scala
@@ -13,10 +13,11 @@ import c2c.webspecs.{XmlValue, Response, IdValue, GetRequest}
  */
 
 class CheSchemaMetadatas  extends GeonetworkSpecification {  def is =
+                                                                            sequential                  ^
   "This specification tests using the iso19139.CHE schema"                  ^ Step(setup)               ^
-    "Inserting a CHE metadata"                                              ^ importISO19139CCHE.give  ^
+    "Inserting a CHE metadata"                                              ^ importISO19139CCHE.give   ^
     "Should suceed with a 200 response"                                     ^ import200Response         ^
-       "Should return the id of the inserted MD"                            ^ getInsertedMdId.then           ^
+       "Should return the MD"                                               ^ getInsertedMd.then      ^
                                                                             Step(tearDown)
 
   val importISO19139CCHE = (_:String) => {
@@ -31,14 +32,27 @@ class CheSchemaMetadatas  extends GeonetworkSpecification {  def is =
   val import200Response = a200ResponseThen.narrow[Response[IdValue]]
 
 
-  val getInsertedMdId = (idRes:Response[IdValue]) => {
+  val getInsertedMd = (idRes:Response[IdValue]) => {
     val xmlResponse = GetRequest("xml.metadata.get", "id" -> idRes.value.id)(None)
     xmlResponse.value.withXml{md =>
 
-      val node = (md \\ "citation"  \ "CI_Citation" \ "title" \ "CharacterString").text  ;
-      val abstractText = (md \\ "abstract").text.trim
+      val node = (md \\ "citation"  \ "CI_Citation" \ "title" \ "CharacterString").text
 
-        ((node must_== "COmprehenisve Test") and (abstractText must_== "xx"))
+      val abstractText = (md \\ "abstract" \\ "CharacterString").text.trim
+      val abstractLocalisedENtext = (md \\ "abstract" \\ "LocalisedCharacterString" find (n =>  (n \\ "@locale").text == "#EN")).get.text
+      val abstractLocalisedDEtext = (md \\ "abstract" \\ "LocalisedCharacterString" find (n =>  (n \\ "@locale").text == "#DE")).get.text
+
+      // Test che: fields
+      val cheLocalisedUrl = (md \ "contact" \ "CHE_CI_ResponsibleParty" \ "contactInfo" \ "CI_Contact" \
+        "onlineResource" \ "CI_OnlineResource" \ "linkage" \ "PT_FreeURL" \ "URLGroup" \ "LocalisedURL").text
+
+//        ((node must_== "COmprehenisve Test") and (abstractText must_== "xx"))
+        ((node must_== "FR Title")
+          and (abstractText must_== "FR abstract")
+          and (cheLocalisedUrl must_== "http://www.awnl.llv.li")
+          and (abstractLocalisedENtext must_== "EN  abstract")
+          and (abstractLocalisedDEtext must_== "DE  abstract")
+          )
     }
   }
 }
