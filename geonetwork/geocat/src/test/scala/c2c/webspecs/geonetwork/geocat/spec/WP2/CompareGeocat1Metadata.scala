e6bcdb8baf7ed32e26757d58c9cb56dae7c7a9a4
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-07-15 17:26:54 +0200

    WP2 : fixing geonet:info removal before MD comparison

--- geonetwork/geocat/src/test/scala/c2c/webspecs/geonetwork/geocat/spec/WP2/CompareGeocat1Metadata.scala
+++ geonetwork/geocat/src/test/scala/c2c/webspecs/geonetwork/geocat/spec/WP2/CompareGeocat1Metadata.scala
@@ -9,68 +9,93 @@ import xml.NodeSeq
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 import org.specs2.matcher.MustThrownMatchers
-import scala.xml.transform.BasicTransformer
+import scala.xml.transform._
 import scala.xml.Node
+
 
 @RunWith(classOf[JUnitRunner])
 class CompareGeocat1Metadata extends GeonetworkSpecification with MustThrownMatchers {
-  def is =
+	def is =
 
-    "This spec will compare GeoCat1 metadata to the migrated ones" ! doDiff
+			"This spec will compare GeoCat1 metadata to the migrated ones" ! doDiff
 
-  def doDiff = {
-    val prodUrl = "http://www.geocat.ch/geonetwork/srv/fra/"
+			def doDiff = {
+			val prodUrl = "http://www.geocat.ch/geonetwork/srv/fra/"
 
-    val geocatLogin = FormPostRequest(prodUrl + "login",
-      "username" -> "admin",
-      "password" -> Properties("geocat.admin.password"))
-    ExecutionContext.withDefault { prodContext =>
-      implicit val c = prodContext
-      geocatLogin(Nil)
+					val geocatLogin = FormPostRequest(prodUrl + "login",
+							"username" -> "admin",
+							"password" -> Properties("geocat.admin.password"))
+							ExecutionContext.withDefault { prodContext =>
+							implicit val c = prodContext
+							geocatLogin(Nil)
 
-      val filter = PropertyIsEqualTo("_isHarvested", "n")
-      val xmlRet = XmlPostRequest(prodUrl + "csw", CswXmlUtil.getRecordsXml(filter = filter.xml, maxRecords = 10000))(Nil).value.getXml
-      println(xmlRet)
+							val filter = PropertyIsEqualTo("_isHarvested", "n")
+							val xmlRet = XmlPostRequest(prodUrl + "csw", CswXmlUtil.getRecordsXml(filter = filter.xml, maxRecords = 10000))(Nil).value.getXml
+							println(xmlRet)
 
-      val numRecords = (xmlRet \\ "SearchResults" \@ "numberOfRecordsMatched").head.toInt
+							val numRecords = (xmlRet \\ "SearchResults" \@ "numberOfRecordsMatched").head.toInt
 
-      val pageSize = 2
+							val pageSize = 2
 
-      for (index <- 1 to numRecords by pageSize; if index < 4) {
-        val xmlCurrent = XmlPostRequest(prodUrl + "csw",
-          CswXmlUtil.getRecordsXml(startPosition = index,
-            filter = filter.xml,
-            resultType = ResultTypes.resultsWithSummary,
-            outputSchema = CheRecord,
-            maxRecords = pageSize))(Nil).value.getXml
-        //println(xmlCurrent)
-        val mds = (xmlCurrent \\ "MD_Metadata") ++ (xmlCurrent \\ "CHE_MD_Metadata")
-        assert(mds.size == pageSize || mds.size == numRecords % pageSize)
-        mds.foreach(compareToNewVersion)
-      }
-      success
-    }
+							for (index <- 1 to numRecords by pageSize; if index < 4) {
+								val xmlCurrent = XmlPostRequest(prodUrl + "csw",
+										CswXmlUtil.getRecordsXml(startPosition = index,
+										filter = filter.xml,
+										resultType = ResultTypes.resultsWithSummary,
+										outputSchema = CheRecord,
+										maxRecords = pageSize))(Nil).value.getXml
+										//println(xmlCurrent)
+										val mds = (xmlCurrent \\ "MD_Metadata") ++ (xmlCurrent \\ "CHE_MD_Metadata")
+										assert(mds.size == pageSize || mds.size == numRecords % pageSize)
+										mds.foreach(compareToNewVersion)
+							}
+							success
+			}
 
-  }
 
-  def compareToNewVersion(md: Node) = {
-    val mdIds = (md \\ "info" \ "id").map(_.text)
-    println(mdIds mkString "\n")
+	}
 
-    assert(mdIds.size == 1)
+	def compareToNewVersion(md: Node) = {
+		val mdIds = (md \\ "info" \ "id").map(_.text)
+				println(mdIds mkString "\n")
 
-    val mdFromGeocatWithoutInfoInfoXml = RemoveInfo(md)
-    
-    GetRawMetadataXml(Id(mdIds.head)).value.getXml must beEqualToIgnoringSpace(mdFromGeocatWithoutInfoInfoXml)
-  }
-  
-  object RemoveInfo extends BasicTransformer{
-    override def transform(n:Node) = {
-      if(n.namespace == "geonet") {
-        Nil
-      } else {
-        n
-      }
-    }
-  }
+				assert(mdIds.size == 1)
+
+				val geonetInfo = md \\ "info" // filter {e => e.namespace == "geonet"}
+
+				object RemoveGeonetInfo extends RewriteRule {
+			override def transform(n: Node): Seq[Node] ={ 
+					if (geonetInfo contains n) Array[Node]()
+					else n
+			}
+		}
+		val mdFromGeocatWithoutInfoInfoXml =  new RuleTransformer(RemoveGeonetInfo)(md)
+
+
+				config.adminLogin(None)
+				val mdFromLocalCatalogue = GetRawMetadataXml(Id(mdIds.head)).value.getXml
+
+				mdFromLocalCatalogue must beEqualToIgnoringSpace(mdFromGeocatWithoutInfoInfoXml)
+
+	}
+
+
+
+
+
+
+
+	//  object RemoveInfo extends RewriteRule{
+	//    override def transform(n:Node) = {
+	//      val geonetInfoFragments = n  filter {e => e.namespace == "geonet"}
+	//      if (geonetInfoFragments contains n) Array[Node]()
+	//      else n
+	//      if(n.namespace == "geonet") {
+	//        Nil
+	//      } else {
+	//        n
+	//      }
+	//    }
+	//    }
+	//  }
 }
