940ba90c88cf25de4a3ea83dbf27b15757e3fe1a
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-07-19 10:32:48 +0200

    fixed changes after merging all projects into a single project

---
+++ core/src/test/scala/c2c/webspecs/geonetwork/geocat/GetEditingMetadataXml.scala
@@ -0,0 +1,28 @@
+package c2c.webspecs
+package geonetwork
+package geocat
+
+/**
+ * Get MetadataXML with metadata information like xlinks etc...
+ * 
+ * @author jeichar
+ */
+object GetEditingMetadataXml 
+	extends AbstractGetRequest[Id,MetadataValue]("xml.metadata.get", 
+        SelfValueFactory[Id,MetadataValue](), 
+        InP("id", (_:Id).id),
+        SP("addEditing" -> true)
+        ) 
+    with ValueFactory[Id,MetadataValue] {
+    def createValue[A <: Id, B >: MetadataValue]
+            (request:Request[A,B],
+             in:Id,
+             rawValue:BasicHttpValue,
+             executionContext:ExecutionContext) = {
+    new MetadataValue {
+      def basicValue = rawValue
+      val id = in.id
+    }
+  }
+  
+}
