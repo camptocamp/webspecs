9e23c18d2ad3d6a64b2446e79466381b42c7acc0
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-07-19 16:28:30 +0200

    Pair programming session w/ JER
    WP6 and WP2 mainly

--- core/src/test/scala/c2c/webspecs/geonetwork/csw/CswGetByFileId.scala
+++ core/src/test/scala/c2c/webspecs/geonetwork/csw/CswGetByFileId.scala
@@ -7,8 +7,9 @@ import ResultTypes._
 case class CswGetByFileId(
     fileId:String, 
     outputSchema:OutputSchemas.OutputSchema,
-    resultType:ResultType=results)
-  extends AbstractXmlPostRequest[Any,XmlValue]("csw", XmlValueFactory) {
+    resultType:ResultType=results,
+    url:String="csw")
+  extends AbstractXmlPostRequest[Any,XmlValue](url, XmlValueFactory) {
 
   def xmlData = CswXmlUtil.getByIdXml(fileId,resultType,outputSchema)
   override def toString() = "CswGetByFileId("+fileId+","+outputSchema+")"
