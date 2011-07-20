821ad4689fb30a81222a88c5c363eb5dabdb1ca2
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-07-20 20:41:48 +0200

    updated process tests so they don't pollute the database and now pass

--- core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP3/ProcessImportedMetadataSpec.scala
+++ core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP3/ProcessImportedMetadataSpec.scala
@@ -12,32 +12,32 @@ import scala.xml.Elem
 import scala.xml.MetaData
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
+import scala.xml.NodeSeq
 
 @RunWith(classOf[JUnitRunner]) 
 class ProcessImportedMetadataSpec extends GeocatSpecification { def is =
   "Shared Object Processing of Imported Metadata".title ^ Step(setup) ^
   "This specification tests how imported metadata are processed for shared objects"             ^
       "When a ${data} metadata is imported"                                                     ^ importMetadata.toGiven ^
-      "The import must complete successfully"                                                   ^ a200ResponseThen.narrow[Response[(Node,Node)]]  ^
-      "All ${extents} must have xlink:href attributes"                                          ^ xlinked.toThen ^
-      "All ${contact} must have xlink:href attributes"                                          ^ xlinked.toThen ^
-      "All ${descriptiveKeywords} must have xlink:href attributes"                              ^ xlinked.toThen ^
-      "All ${citedResponsibleParty} must have xlink:href attributes"                            ^ xlinked.toThen ^
-      "All ${pointOfContact} must have xlink:href attributes"                                   ^ xlinked.toThen ^
-      "All ${resourceFormat} must have xlink:href attributes"                                   ^ xlinked.toThen ^
-      "All ${userContactInfo} must have xlink:href attributes"                                  ^ xlinked.toThen ^
-      "All ${distributionFormat} must have xlink:href attributes"                               ^ xlinked.toThen ^
-      "All ${distributorContact} must have xlink:href attributes"                               ^ xlinked.toThen ^
-      "All ${sourceExtent} must have xlink:href attributes"                                     ^ xlinked.toThen ^
-      "All ${spatialExtent} must have xlink:href attributes"                                    ^ xlinked.toThen ^
-      "The imported metadata must have same elements as original data (in this case)"           ^ sameElements.toThen ^
+      "The import must complete successfully"                                                   ^ a200ResponseThen.narrow[TestData]  ^
+      "All ${extent} must have ${several} xlink:href attributes"                                          ^ xlinked.toThen ^
+      "All ${contact} must have ${several}  xlink:href attributes"                                          ^ xlinked.toThen ^
+      "All ${descriptiveKeywords} must have ${several} xlink:href attributes"                              ^ xlinked.toThen ^
+      "All ${citedResponsibleParty} must have ${several} xlink:href attributes"                            ^ xlinked.toThen ^
+      "All ${pointOfContact} must have ${several} xlink:href attributes"                                   ^ xlinked.toThen ^
+      "All ${resourceFormat} must have ${several} xlink:href attributes"                                   ^ xlinked.toThen ^
+      "All ${userContactInfo} must have ${several} xlink:href attributes"                                  ^ xlinked.toThen ^
+      "All ${distributionFormat} must have ${several} xlink:href attributes"                               ^ xlinked.toThen ^
+      "All ${distributorContact} must have ${several} xlink:href attributes"                               ^ xlinked.toThen ^
+      "All ${sourceExtent} must have ${1} xlink:href attributes"                                     ^ xlinked.toThen ^
+      "All ${spatialExtent} must have ${2} xlink:href attributes"                                    ^ xlinked.toThen ^
                                                                                                   endp ^
                                                                                                   Step(tearDown)
 
 
-                              
+   type TestData = Response[(NodeSeq,NodeSeq,NodeSeq)]
 
-   val importMetadata:(String) => Response[(Node,Node)] = (s:String) => {
+   val importMetadata:(String) => TestData = (s:String) => {
      val (xmlString,data) = extract1(s) match {
        case "data" =>
           ResourceLoader.loadDataFromClassPath("/geocat/data/comprehensive-iso19139che.xml",classOf[ProcessImportedMetadataSpec],uuid)
@@ -48,31 +48,28 @@ class ProcessImportedMetadataSpec extends GeocatSpecification { def is =
      val originalXml = XML.loadString(xmlString)
      val ImportRequest = ImportMetadata.findGroupId(data,NONE,false)
      
-     val response = (UserLogin then ImportRequest then GetEditingMetadataXml startTrackingThen DeleteMetadata)(None)
+     val importResponse = (UserLogin then ImportRequest)(None)
+     val id = importResponse.value
+     val mdWithXLinks =  GetEditingMetadataXml(id).value.getXml
+     val mdWithoutXLinks = GetRawMetadataXml(id).value.getXml
+     val deleteResponse = DeleteMetadata(id)
+     assert(deleteResponse.basicValue.responseCode == 200, "Failed to delete imported metadata")
 
-     response._1.map(mv => (originalXml, mv.getXml.asInstanceOf[Node]))
-   }
- 
-   val sameElements = (r:Response[(Node,Node)]) => {
-     val (originalMd, importedMd) = r.value
-     val noxlinks = RemoveHrefs.apply(importedMd)
-     noxlinks must beEqualToIgnoringSpace(originalMd)
+     importResponse.map(mv => (originalXml, mdWithoutXLinks, mdWithXLinks))
    }
    
-   val xlinked = (r:Response[(Node,Node)],s:String) => {
-     val (_, importedMd) = r.value
-     val node = extract1(s)
+   def xlinked = (r:TestData,s:String) => {
+     val (_, _ , mdWithXLinks) = r.value
+     val (node, number) = extract2(s)
      
-     importedMd \\ node \@ "xlink:href" must not beEmpty
+     val elem = mdWithXLinks \\ node
+     val countMatcher = if(number == "several") {
+       (elem \@ "xlink:href" aka "href" must not beEmpty)
+     } else {
+       (elem \@ "xlink:href" aka "href" must haveSize(number.toInt) )
+     }
+     (elem aka (node+" element") must not beEmpty) and
+     	countMatcher 
    }
    
-   object RemoveHrefs extends BasicTransformer {
-     override def transform(n:Node) = n match {
-       case e:Elem =>
-         val xlinklessAtts = e.attributes filterNot {_.key startsWith "xlink:"} reduce {(acc,next) => acc.copy(next)}
-         e.copy(attributes = xlinklessAtts)
-       case _ => 
-         n
-     }
-   }
 }
