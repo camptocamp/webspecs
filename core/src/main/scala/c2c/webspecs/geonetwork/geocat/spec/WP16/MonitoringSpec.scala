270da8884fbe6fa1af69556af82559954ec9ec4d
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-07-27 16:29:19 +0200

    WP16: Monitoring
    Adds a statement to test if the monitoring.report service is available
    (same service as monitoring, but outputs its report as HTML)

--- core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP16/MonitoringSpec.scala
+++ core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP16/MonitoringSpec.scala
@@ -12,15 +12,16 @@ import c2c.webspecs.geonetwork.UserProfiles
 
 @RunWith(classOf[JUnitRunner]) 
 class MonitoringSpec extends GeocatSpecification(UserProfiles.Admin) {  def is =
-	"Xsl custom metadata XML output".title 								^ Step(setup)                     ^
-			"Login as admin"											^ Step(config.adminLogin()) 	   ^
-			"Get the results of the monitoring webservice"				^ Step(getMonitoringXmlReport)    ^
-			"Checking ${db} status"										! checkServicesStatus		 ^
-			"Checking ${cswService_getrecords} status"					! checkServicesStatus		 ^
-			"Checking ${freediskService} status"						! checkServicesStatus		 ^
-			"Checking ${cswService_capabilities} status"				! checkServicesStatus		 ^
-//			"Check ${print_service} status"								! checkServicesStatus		 ^
-																		 end ^ Step(tearDown)
+	"Xsl custom metadata XML output".title 														^ Step(setup)                     ^
+			"Login as admin"																	^ Step(config.adminLogin()) 	  ^
+			"Check if the monitor webservice which outputs its report as HTML is available"		^ Step(testMonitoringReport)    ^
+			"Get the results of the monitoring webservice"										^ Step(getMonitoringXmlReport)    ^
+			"Checking ${db} status"																! checkServicesStatus		      ^
+			"Checking ${cswService_getrecords} status"											! checkServicesStatus		      ^
+			"Checking ${freediskService} status"												! checkServicesStatus		      ^
+			"Checking ${cswService_capabilities} status"										! checkServicesStatus		      ^
+//			"Check ${print_service} status"														! checkServicesStatus		      ^
+																		 						end ^ Step(tearDown)
 			
 			
 			
@@ -29,6 +30,11 @@ class MonitoringSpec extends GeocatSpecification(UserProfiles.Admin) {  def is =
 	xmlMonitoringResult must haveA200ResponseCode
   	val response = xmlMonitoringResult.value.getXml
     response
+  }
+
+  def testMonitoringReport =  {
+    val ret = GetRequest("monitoring.report")(Nil)
+    ret must haveA200ResponseCode
   }
 
   def checkServicesStatus = (desc:String) => {
