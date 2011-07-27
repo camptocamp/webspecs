224c4197c1884b0dc45bc36b320c7eca05b4c2d3
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-07-27 16:22:39 +0200

    WP16
    Adding a spec to test the XML output of the monitoring service

---
+++ core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP16/MonitoringSpec.scala
@@ -0,0 +1,39 @@
+package c2c.webspecs
+package geonetwork
+package geocat
+package spec.WP16
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.specification.Step
+
+import c2c.webspecs.geonetwork.UserProfiles
+
+
+@RunWith(classOf[JUnitRunner]) 
+class MonitoringSpec extends GeocatSpecification(UserProfiles.Admin) {  def is =
+	"Xsl custom metadata XML output".title 								^ Step(setup)                     ^
+			"Login as admin"											^ Step(config.adminLogin()) 	   ^
+			"Get the results of the monitoring webservice"				^ Step(getMonitoringXmlReport)    ^
+			"Checking ${db} status"										! checkServicesStatus		 ^
+			"Checking ${cswService_getrecords} status"					! checkServicesStatus		 ^
+			"Checking ${freediskService} status"						! checkServicesStatus		 ^
+			"Checking ${cswService_capabilities} status"				! checkServicesStatus		 ^
+//			"Check ${print_service} status"								! checkServicesStatus		 ^
+																		 end ^ Step(tearDown)
+			
+			
+			
+  lazy val getMonitoringXmlReport = {
+  	val xmlMonitoringResult = GetRequest("monitoring")(Nil)
+	xmlMonitoringResult must haveA200ResponseCode
+  	val response = xmlMonitoringResult.value.getXml
+    response
+  }
+
+  def checkServicesStatus = (desc:String) => {
+    val strStatus = (getMonitoringXmlReport \\ extract1(desc) \ "status").text
+    strStatus must_== "ok"
+  }
+
+}
