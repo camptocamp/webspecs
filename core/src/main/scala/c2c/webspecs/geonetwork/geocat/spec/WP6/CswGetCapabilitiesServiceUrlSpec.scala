24c1b9d9b49a7c187862472fab5948cbac276afb
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-08-02 17:25:11 +0200

    WP6: GetCapabilities
    Removing unused imports

--- core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP6/CswGetCapabilitiesServiceUrlSpec.scala
+++ core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP6/CswGetCapabilitiesServiceUrlSpec.scala
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 import org.specs2.specification.Step
 
-import c2c.webspecs.geonetwork.ImportStyleSheets.NONE
 import c2c.webspecs.geonetwork.csw._
 import c2c.webspecs.GetRequest
 import c2c.webspecs.geonetwork.UserProfiles
