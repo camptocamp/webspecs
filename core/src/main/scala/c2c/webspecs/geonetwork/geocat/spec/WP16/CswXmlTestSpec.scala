c7bcb22a1dd2f1f843a6a42d8cf8a2189888ea19
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-08-02 15:30:24 +0200

    WP16: CSW test
    First shot on the Csw XML tests provided by GeoNetwork trunk

---
+++ core/src/main/scala/c2c/webspecs/geonetwork/geocat/spec/WP16/CswXmlTestSpec.scala
@@ -0,0 +1,81 @@
+package c2c.webspecs
+package geonetwork
+package geocat
+package spec.WP16
+
+import org.junit.runner.RunWith
+import scala.xml._
+import org.specs2.runner.JUnitRunner
+import org.specs2.specification.Step
+
+import c2c.webspecs.geonetwork.UserProfiles
+
+
+@RunWith(classOf[JUnitRunner]) 
+class CswXmlTestSpec extends GeocatSpecification(UserProfiles.Admin) {  def is =
+	"GeoNetwork-trunk XML testsuite for CSW server".title 							        ^ Step(setup)                     ^
+	  "Loads a sample metadata"      	 	                			  	                ^ Step(importMetadataId)          ^
+    	"Process test using XML file : ${csw-DescribeRecordWithMD_Metadata}"                ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-DescribeRecordWithMultipleTypeName}"           ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-DescribeRecord}"                               ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetCapabilitiesSections}"                      ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetCapabilities}"                              ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetDomainParameterName}"                       ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetDomainPropertyName}"                        ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordByIdFraIsoRecord}"                    ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordByIdIsoRecord}"                       ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordById}"                                ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsCQLAny}"                             ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsCQLEquals}"                          ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsElementName}"                        ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterGeoBbox2Equals}"               ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterGeoBboxEquals}"                ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterGeoBboxForMoreThan180degrees}" ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterGeoBoxIntersects}"             ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterGeoBox}"       				! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterGeoEnvelope}"        			! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterIsEqualToOnKeyword}"        	! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterIsEqualToOnProtocol}"        	! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterIsEqualToPhraseOnTitle}"       ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterIsLikeOnAny}"        			! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterIsLikePhraseOnAbstract}"       ! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterRangeCswIsoRecord}"        	! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsFilterService}"        				! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilterCswIsoRecord}"        		! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilterFraIsoRecord}"        		! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilterIsoRecord}"        			! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilterOwn}"        				! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilterResultsWithSummary}"        	! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilterResults}"        			! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilterValidate}"        			! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsNoFilter}"        					! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-GetRecordsSortBy}"        						! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-Harvest}"        								! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-OwsException}"        							! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-TransactionDelete}"        					! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-TransactionInsert}"        					! ProceedXmlTest		          ^
+  	    "Process test using XML file : ${csw-TransactionUpdate}"        					! ProceedXmlTest		          ^
+	  "Delete the inserted metadata"									                    ^ Step(deleteMetadata)            ^
+																		 			        end ^ Step(tearDown)
+			
+  lazy val importMetadataId = {
+    val importMdRequest = ImportMetadata.defaults(uuid, "/geocat/data/metadata.iso19139.che.xml",true, getClass)._2
+	val md = (importMdRequest then GetRawMetadataXml)(ImportStyleSheets.NONE).value.getXml
+	val response = (md \\ "fileIdentifier").text.trim
+	response
+  }
+
+    def deleteMetadata = {
+		  GetRequest("metadata.delete", ("uuid" -> importMetadataId))(Nil)
+
+    }
+
+  def ProceedXmlTest = (desc:String) => {
+    val xmlFile = extract1(desc) + ".xml"
+    val (xmlResource,_) = ResourceLoader.loadDataFromClassPath("/geocat/csw-xml-tests/"+xmlFile, getClass, uuid)
+    val cswTestRequest = XmlPostRequest("csw", XML.loadString(xmlResource))()
+    cswTestRequest.value.getXml
+    (cswTestRequest must haveA200ResponseCode)
+  }
+
+}
