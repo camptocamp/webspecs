2bbf0850ddc0812ca88a49f6a67ce595a3c11676
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-08-24 11:37:51 +0200

    Added a maxFeatures test

--- ign/src/test/scala/c2c/webspecs/geoserver/IGNSpec.scala
+++ ign/src/test/scala/c2c/webspecs/geoserver/IGNSpec.scala
@@ -7,13 +7,22 @@ import org.specs2.execute.Result
 
 @RunWith(classOf[JUnitRunner])
 class IGNSpec extends GeoserverSpecification {
-  // TODO test WFSDispatcher with all of these as well as just /wfs
+
   def isImpl = 
     "ignGetFeatureTests".title ^
     	"This Spec test WFS for IGN get Feature API" ^
+    	"Wfs Max Features should limit the number of features loaded" ! maxFeatures ^
     	"Wfs Fid Filter must retrieve exactly one feature" ! fidFilter
     	
     	
+   def maxFeatures = {
+    val response = GetWfsRequest("2.0.0", "GetFeature", "typeName" -> "au:AdministrativeUnit", "maxFeatures" -> 1).execute()
+    val xmlData = response.value.getXml
+    
+    (response must haveA200ResponseCode) and
+      (response.value.getXml \\ "member" must haveSize(1))
+    
+  } 	
    def fidFilter = {
     val filter = <fes:ResourceId rid="FR2100000000"/>
     val response = new GetFeatureRequest("au:AdministrativeUnit", filter).execute()
