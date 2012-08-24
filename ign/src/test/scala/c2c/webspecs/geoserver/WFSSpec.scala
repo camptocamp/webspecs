452541424b84b1d622d9d0b9e109a5f32443652e
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-08-24 12:12:22 +0200

    fixed WFS basic tests

--- ign/src/test/scala/c2c/webspecs/geoserver/WFSSpec.scala
+++ ign/src/test/scala/c2c/webspecs/geoserver/WFSSpec.scala
@@ -14,8 +14,8 @@ class WFSSpec extends GeoserverSpecification {
       "This Spec test WFS API" ^
       "Wfs getCapabilities 1.0.0 must be valid" ! capabilities100 ^
       "Wfs getCapabilities 1.1.0 must be valid" ! capabilities110 ^
-      "Wfs DescribeFeatureType must be valid" ! describeFeatureType ^
-      "Wfs GetFeature can get at least one features" ! getFeature
+      "Wfs getCapabilities 2.0.0 must be valid" ! capabilities200 ^
+      "Wfs 2.0.0 DescribeFeatureType must be valid for ${au:AdministrativeUnit}" ! describeFeatureType
 
   def capabilities100 = {
     val response = GetWfsRequest("1.0.0", "GetCapabilities").execute()
@@ -52,45 +52,33 @@ class WFSSpec extends GeoserverSpecification {
       (response.value.getXml must \\("FeatureType")) and
       (response.value.getXml must \\("Filter_Capabilities"))
   }
+  def capabilities200 = {
+    val response = GetWfsRequest("2.0.0", "GetCapabilities").execute()
+    (response must haveA200ResponseCode) and
+      (response.value.getXml must \\("WFS_Capabilities")) and
+      (response.value.getXml must \\("ServiceIdentification")) and
+      (response.value.getXml must \\("ServiceProvider")) and
+      (response.value.getXml must \\("ServiceContact")) and
+      (response.value.getXml must \\("OperationsMetadata")) and
+      (response must haveOperation("GetCapabilities")) and
+      (response must haveOperation("DescribeFeatureType")) and
+      (response must haveOperation("GetFeature")) and
+      (response must haveOperation("LockFeature")) and
+      (response must haveOperation("GetFeatureWithLock")) and
+      (response must haveOperation("Transaction")) and
+      (response.value.getXml \\ "Get" \@ "xlink:href" must contain("://" + Properties.testServer).forall) and
+      (response.value.getXml \\ "Post" \@ "xlink:href" must contain("://" + Properties.testServer).forall) and
+      (response.value.getXml must \\("FeatureTypeList")) and
+      (response.value.getXml must \\("FeatureType")) and
+      (response.value.getXml must \\("Filter_Capabilities"))
+  }
 
-  lazy val typeNames = {
-    val response = GetWfsRequest("1.1.0", "GetCapabilities").execute()
-    (response.value.getXml \\ "FeatureType" \\ "Name").map(_.text.trim)
+  val describeFeatureType = (description: String) => {
+    val typeName = extract1(description)
+    val response = GetWfsRequest("2.0.0", "DescribeFeatureType", "TypeName" -> typeName).execute()
+    ((response must haveA200ResponseCode) and
+      (response.value.getText must contain("schema")) and
+      (response.value.getXml must \\("schema")))
   }
-  def describeFeatureType = {
-    def describe(typeName: String) = {
-      val response = GetWfsRequest("1.1.0", "DescribeFeatureType", "TypeName" -> typeName).execute()
-      ((response must haveA200ResponseCode) and
-      	(response.value.getText must contain("schema")) and
-      	(response.value.getXml must \\("schema")) and
-      	(response.value.getXml must \\("complexType")) and
-      	(response.value.getXml must \\("extension")) and
-      	(response.value.getXml \\ "complexContent" \ "extension" \ "sequence" must \("element")))
-    }
-    typeNames.foldLeft(success:Result){ (result, next) => result and describe(next)}
-  }
-  def getFeature = {
-    def getFirstFeature(typeName:String) = {
-      val xml = 
-        <wfs:GetFeature 
-    		  service="WFS" 
-    		  version="1.1.0"
-    		  maxFeatures="1"
-    		  xmlns:topp="http://www.openplans.org/topp" xmlns:wfs="http://www.opengis.net/wfs" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs
-    		  http://schemas.opengis.net/wfs/1.1.0/wfs.xsd">
-        <wfs:Query typeName={typeName}>
-        </wfs:Query>
-      </wfs:GetFeature>
-      val featureTypeName = typeName.split(":").toList match {
-        case _ :: name :: Nil => name
-        case name :: Nil => name
-        case _ => throw new IllegalStateException("boom")
-      }
-      val response = XmlPostRequest(GeoserverRequests.url(None, "wfs"),xml).execute()
-      (response.value.getXml must \\("FeatureCollection")) and
-      	(response.value.getXml must \\("featureMembers")) and
-      	(response.value.getXml \\ featureTypeName must haveSize(1))
-    }
-    typeNames.foldLeft(success:Result){ (result, next) => result and getFirstFeature(next)}
-  }
+  
 }
