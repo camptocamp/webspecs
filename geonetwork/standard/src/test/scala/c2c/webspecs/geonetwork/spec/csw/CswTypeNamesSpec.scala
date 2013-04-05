2cb8dbb9f3881f558b292942fff5d21ffcd64c9e
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-04-05 15:51:46 +0200

    added test against endpoint

--- geonetwork/standard/src/test/scala/c2c/webspecs/geonetwork/spec/csw/CswTypeNamesSpec.scala
+++ geonetwork/standard/src/test/scala/c2c/webspecs/geonetwork/spec/csw/CswTypeNamesSpec.scala
@@ -9,14 +9,14 @@ import org.specs2.specification.Step
 import c2c.webspecs.geonetwork.csw._
 import c2c.webspecs.geonetwork.GetRawMetadataXml
 
-
 @RunWith(classOf[JUnitRunner])
 class CswTypeNamesSpec extends GeonetworkSpecification {
-	def is = {
-	  	"Import a metadata" ^ Step(setup) ^ Step(importMetadataId) ^
-	  	"Make request with multiple typenames, the metadata should be returned" ! request ^
-	  																			  Step(tearDown)
-	}
+  def is = {
+    "Import a metadata" ^ Step(setup) ^ Step(importMetadataId) ^
+      "Make request with multiple typenames, the metadata should be returned" ! request ^
+      "Make request with multiple typenames, on a csw endpoint" ! requestCswEndpoint ^
+      Step(tearDown)
+  }
 
   lazy val importMetadataId = {
     val mdId = importMd(1, "/geonetwork/data/multilingual-metadata.iso19139.xml", uuid.toString).head
@@ -27,7 +27,14 @@ class CswTypeNamesSpec extends GeonetworkSpecification {
   }
 
   def request = {
-    val results = CswGetRecordsRequest(Nil, typeNames = List("csw:Record","gmd:MD_Metadata")).execute()
+    val results = CswGetRecordsRequest(Nil, typeNames = List("csw:Record", "gmd:MD_Metadata")).execute()
+    println(results.value.getXml)
+    val matches = (results.value.getXml \ "SearchResults" \ "@numberOfRecordsMatched")
+    (matches must not(beEmpty)) and (matches.text.toInt must_== 1)
+  }
+
+  def requestCswEndpoint = {
+    val results = CswGetRecordsRequest(Nil, url="csw-integration-test", typeNames = List("csw:Record", "gmd:MD_Metadata")).execute()
     println(results.value.getXml)
     val matches = (results.value.getXml \ "SearchResults" \ "@numberOfRecordsMatched")
     (matches must not(beEmpty)) and (matches.text.toInt must_== 1)
